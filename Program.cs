c4f335f5144b8f02097116c818a32710897f8402
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-08-03 15:29:29 +0530

    Assignment22

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Source source = new Source
+            {
+                Id = 100,
+                Name = "Sam",
+                Age = 24,
+                Occupation = "Associate Software Engineer"
+
+            };
+
+            Destination destination = new Destination();
+
+            PropertyMapper.MapProperties(source, destination);
+
+            Console.WriteLine("Mapped properties in the Destination class:");
+            Console.WriteLine($"Id: {destination.Id}");
+            Console.WriteLine($"Name: {destination.Name}");
+            Console.WriteLine($"Occupation: {destination.Occupation}");
+            Console.ReadKey();
+        }
+    }
+}
